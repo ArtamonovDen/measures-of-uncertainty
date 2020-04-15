a9427918ef785dffeedea3c3e4586e4b22dc01b1
Denis <brightcolors46@gmail.com>
2020-04-15 22:41:02 +0300

    Add sample network

--- k_scripts/network_scripts.py
+++ k_scripts/network_scripts.py
@@ -2,18 +2,38 @@
 import numpy as np
 import networkx as nx
 
-def create_reference_network(C):
+def create_network(W):
     '''
-        Create reference network by given Correlation matrix 
-        C: Correlation matrix
-
+        Create weighted graph without loob by weight matrix. Weight matrix is correlation matrix
+        W: weights
     '''
-    # TODO Add nodes' names
-    edjes = [ (*nodes,weight) for nodes,weight in np.ndenumerate(C)]
+     # TODO Add nodes' names ?
+    edjes = [ (*nodes,weight) for nodes,weight in np.ndenumerate(W)]
     g = nx.Graph()
     g.add_weighted_edges_from(edjes)
     g.remove_edges_from(nx.selfloop_edges(g))
     return g
+
+
+def create_sample_network(sampler, sampler_params, n):
+    '''
+        Create sample network from given distribution
+        sampler: callable generator like np.random.multivariate_normal
+        sampler_params: params to run sampler with (like mean and covariation matrix)
+        n: number of samples
+
+        Example:
+        a = [0]*N
+        Cov = R.cov()
+        n_sample = np.random.multivariate_normal(mean=a, cov=Cov, size = 10)
+    '''
+
+    n_sample = sampler(*sampler_params, size=n)
+    C = np.corrcoef(n_sample.T)
+    return create_network(C)
+
+
+
 
 def build_MST(g):
     '''
