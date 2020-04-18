16bb16f939c6d3d08836550d0426cc8923af8353
Denis <brightcolors46@gmail.com>
2020-04-18 17:41:00 +0300

    Add E_measures_MST

--- k_scripts/measures_scripts.py
+++ k_scripts/measures_scripts.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from networkx.algorithms.operators import difference
+import network_scripts as ns
 
 def E_measure():
     '''
@@ -35,6 +36,17 @@ def error_type_II(ref_struct, sample_struct):
     '''
     pass
 
-def error_type_II():
+def E_measure_MST(ref_network, sample_network):
     '''
+        Uncertainty measuring of MST filtering structure.
+        For MST E_measure = R_measure, and # of errors of both types are equals.
+        So measure for MST is just E_measure = E(X) = 1/(N-1)*error_type_I
+
+        Return: the value of statistical uncertainty for MST for given n
     '''
+
+    #TODO TEST IT!
+
+    ref_mst = ns.build_MST(ref_network)
+    sample_mst = ns.build_MST(sample_network)
+    return difference(sample_mst, ref_mst).number_of_edges() # Edges presented in sample_mst but not in  ref_mst - I type error
