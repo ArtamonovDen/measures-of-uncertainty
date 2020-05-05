314097b32aa129e276fa9c30b1c3a06a7e025668
Denis <brightcolors46@gmail.com>
2020-05-05 15:40:40 +0300

    Small fix in name

--- k_scripts/measures.py
+++ k_scripts/measures.py
@@ -32,7 +32,7 @@ def E_measure_MST(reference_network, sampler, sampler_params):
 
     Xs = list()
     ref_mst = ns.build_MST(reference_network)
-    N = ref_network.number_of_nodes() 
+    N = reference_network.number_of_nodes() 
     for i in range(1000): # Calculate E[X] as mean of different Xs
         sample_network = ns.create_sample_network(sampler, sampler_params)    
         Xs.append(
